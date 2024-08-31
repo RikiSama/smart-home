68610603e372d1c94074b141a8bb8171699de83a
ZwelNyiNyarHtut <zwelnyinyinyar@gmail.com>
2024-08-31 10:21:56 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    "./assets/**/*.{jpg,jpeg,png,svg,avif,webp}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
